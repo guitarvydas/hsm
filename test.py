0cbaddffb2fbd1039c91ba81dbbc9040d5539f1a
Paul Tarvydas <guitarvydas@gmail.com>
2022-08-12 09:57:38 -0400

    test power off while in brightness

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ from message import Message
 l = Lamp (None, 'lamp')
 l.inject (Message (l, 'pwr', True, None))
 l.inject (Message (l, 'brightness', True, None))
+l.inject (Message (l, 'brightness', True, None))
+l.inject (Message (l, 'brightness', True, None))
+l.inject (Message (l, 'pwr', True, None))
 l.run ()
 print (l.outputs ())
 
